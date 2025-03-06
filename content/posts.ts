d01f18f189a163f14fbb6fe5bbd8a9415b3950e2
areumpark3 <arempark991@gmail.com>
2025-03-06 13:38:04 +0900

    Update posts.ts
    간단 문구 수정

--- content/posts.ts
+++ content/posts.ts
@@ -3,19 +3,19 @@ import post2 from '../notion-data/127ce18c-fd83-805c-bebd-d6772e18bf02.json'
 
 const posts = [
   {
-    title: "Maximizing the Benefits of BFF Pattern in API Design",
+    title: "작업 내역 상세",
     slug: "api-design-in-bff",
     content: post1,
     date: "2023-10-22",
-    description: "Exploring the shift from domain-centric to data-centric API endpoints after introducing BFF. This article introduces BFF and DDD concepts, then proposes API endpoint design strategies in the context of the BFF pattern.",
+    description: "업무 진행 현황 및 작업 진행 내용 등 업로드 ",
     image: undefined
   },
   {
-    title: "Naver Search Bar UX: Analysis and Implementation Guide",
+    title: "SR 진행 내용",
     slug: "naver-search-bar-ux",
     content: post2,
     date: "2023-10-23",
-    description: "Explore the UX analysis of Naver's search bar and strategies for replicating its features. The post examines the UX triggers, selection of search results, and the refreshing of result lists. It also compares coding approaches for implementing search bar functionalities, aiming for a concise and effective execution.",
+    description: "vSphere 관련 SR 진행한 내용 등 업로드 ",
     image: undefined
   }
 ] as Post[];
