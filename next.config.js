a84a43c95568799cfef49ebdac6d2baf5e3469fa
areumpark3 <arempark991@gmail.com>
2025-03-06 15:08:44 +0900

    Update next.config.js
    수정3

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+      };
+    }
+    return config;
+  },
+};
