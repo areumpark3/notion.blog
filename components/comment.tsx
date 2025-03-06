d4dcaec3bf8053746d0ae282b68954b63e657f2b
areumpark3 <arempark991@gmail.com>
2025-03-06 12:43:43 +0900

    Update comment.tsx
    배포 실패로 인한 수정
    오류 : "File '/opt/buildhome/repo/components/comment.tsx' is not a module."

--- components/comment.tsx
+++ components/comment.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import Giscus from '@giscus/react';
 
-export default function Comment() {
+const Comment: React.FC = () => {
   return (
     <Giscus
       id="comments"
-      repo="areumpark3/notion.blog" // 여기에 GitHub 사용자명/저장소명을 입력하세요
-      repoId="R_kgDOOCzALA" // GitHub에서 제공하는 저장소 ID를 입력하세요
-      category="Announcements" // 그대로 두거나 원하는 카테고리로 변경하세요
-      categoryId="DIC_kwDOOCzALM4Cnm3V" // GitHub Discussions 카테고리 ID를 입력하세요
+      repo="areumpark3/notion.blog"
+      repoId="R_kgDOOCzALA"
+      category="Announcements"
+      categoryId="DIC_kwDOOCzALM4Cnm3V"
       mapping="pathname"
       strict="0"
       reactionsEnabled="1"
@@ -20,6 +20,8 @@ export default function Comment() {
     />
   );
 }
+
+export default Comment;
 
 // To enable comments:
 // 1. Uncomment this component
