f505f20e1f2b621bc4c7d2cb4d9e609146685ae3
GitHub Action <action@github.com>
2025-03-07 11:45:17 +0000

    Update posts from Notion

--- content/posts.ts
+++ content/posts.ts
@@ -1,22 +1,20 @@
-import post1 from '../notion-data/126ce18c-fd83-8023-9ad1-d0e1809b21c3.json'
-import post2 from '../notion-data/127ce18c-fd83-805c-bebd-d6772e18bf02.json'
+import post_1aed9eed7ec680e1b3add4856e48c459 from '../notion-data/post_1aed9eed7ec680e1b3add4856e48c459.json';
+import post_1a1d9eed7ec680228a3ec54b7bd4db1a from '../notion-data/post_1a1d9eed7ec680228a3ec54b7bd4db1a.json';
 
 const posts = [
   {
-    title: "작업 내역 상세",
-    slug: "api-design-in-bff",
-    content: post1,
-    date: "2023-10-22",
-    description: "업무 진행 현황 및 작업 진행 내용 등 업로드 ",
-    image: undefined
+    "title": "윈도유3",
+    "slug": "post-1aed9eed-7ec6-80e1-b3ad-d4856e48c459",
+    "content": post_1aed9eed7ec680e1b3add4856e48c459,
+    "date": "2025-03-07",
+    "description": ""
   },
   {
-    title: "SR 진행 내용",
-    slug: "naver-search-bar-ux",
-    content: post2,
-    date: "2023-10-23",
-    description: "vSphere 관련 SR 진행한 내용 등 업로드 ",
-    image: undefined
+    "title": "윈도우",
+    "slug": "post-1a1d9eed-7ec6-8022-8a3e-c54b7bd4db1a",
+    "content": post_1a1d9eed7ec680228a3ec54b7bd4db1a,
+    "date": "2025-03-07",
+    "description": ""
   }
 ] as Post[];
 
