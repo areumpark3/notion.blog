75b9304cea1b72af652fccd1e8d56ccac5645462
areumpark3 <arempark991@gmail.com>
2025-03-21 12:30:14 +0900

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,14 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
+declare module 'react-notion-x' {
+    export const NotionRenderer: React.ComponentType<{
+      recordMap: import('notion-types').ExtendedRecordMap
+      components?: Partial<import('react-notion-x').NotionComponents>
+      fullPage?: boolean
+      darkMode?: boolean
+    }>
+  }
+
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
